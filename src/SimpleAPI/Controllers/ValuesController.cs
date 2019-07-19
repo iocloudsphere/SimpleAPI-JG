b32ded20b32c93455d9b94b1af170e684768a005
Josias Garcia <josiasgarcia@live.com>
2019-07-19 18:22:56 -0400

    broke unit test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Josias Garcia";
+            return "Max Power";
         }
 
         // POST api/values
